a55b878dcb4f317f18a7be6a20042241de643f3e
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-12-22 23:27:53 +0300

    log in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/header/header";
 import { Route, Routes } from "react-router-dom";
 import Signup from "./components/signup/signup";
+import Login from "./components/logIn/login";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/log" element={<Signup />} />
+        <Route exact path="/logIn" element={<Login />} />
         {/* <Route path="*" element={<Notfound />} /> */}
       </Routes>
     </div>
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -44,7 +44,7 @@ const Header = () => {
               </Link>
             </li>
             <li className="lie">
-              <Link className="link" to="/log">
+              <Link className="link" to="/logIn">
                 log in
               </Link>
             </li>
